84c76f525b0359a7c8ea72aecfbc4a762a35c5f8
ABunian <lwh550318@gmail.com>
2026-03-22 15:14:36 +0800

    fix bug

--- scripts/run_daily_report.sh
+++ scripts/run_daily_report.sh
@@ -4,5 +4,6 @@ export HOME="/home/abu/system_monitor"
 echo "Go to ${HOME}"
 cd ${HOME}
 echo "----------Start reporter----------"
-echo "Log at logs/reporter.log
+echo "Log at logs/reporter.log"
 ./reporter 0 >> ./logs/reporter.log 2>&1
+echo "----------End of reporter---------"
